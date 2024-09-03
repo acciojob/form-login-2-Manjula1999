69ca5054398e93b1703c31e0de89174a55943c68
Manjula1999 <144819647+Manjula1999@users.noreply.github.com>
2024-09-04 00:06:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const submitButton = document.getElemnetById("submitButton");
+const submitButton = document.getElemetById("submitButton");
 const data = { };
 function showAlert() {
 	event.preventDefult();
