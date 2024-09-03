ee0b252e19d17f512dab249781a952d3872fcafc
Manjula1999 <144819647+Manjula1999@users.noreply.github.com>
2024-09-03 23:14:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const submitButton = document.getElemnetById("submitButton");
+const data = { };
+function showAlert() {
+	event.preventDefult();
+	data.firstName = document.getElementById("firstName").value;
+	date.lastName = document.getElementById("lastName").value;
+	data.phoneNumber= document.getElementById("phoneNumber").value;
+	data.Email = document.getElementById("Email").value;
+
+alert( "First Name: " + data.firstName + 
+	  "\nLast Name: " + data.lastName + 
+	  "\nPhone Number: " + data.phoneNumber + 
+	  "\nEmail: " + data.Email ID
+	 );
+}
+
+submitButton.addEventListenr("click", showAlert);
